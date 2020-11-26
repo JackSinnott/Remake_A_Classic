904d0e1c4e747629ac8d73e96136dae4c3deef29
AllexisAlvarico <c00242855@itcarlow.ie>
2020-11-26 23:46:48 +0000

    started working on the cameraView

---
+++ Lab1/Assets/Scripts/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private bool isGrounded;
+    public float jumpPower;
+
+    void Start()
+    {
+        jumpPower = 3.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("d")|| Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
+
