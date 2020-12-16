23e2395afba5f3293263e8fc0a6381e1c79f9505
JackSinnott <45257691+JackSinnott@users.noreply.github.com>
2020-12-16 19:29:10 +0000

    Added player health UI

---
+++ Lab1/Assets/Scripts/Health.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHealthBars;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+
+        if (health > numOfHealthBars)
+        {
+            health = numOfHealthBars;
+        }
+        if (health <= 0)
+        {
+            health = 0;
+        }
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < numOfHealthBars)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            health += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            health -= 1;
+        }
+    }
+}
