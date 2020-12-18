1eda856cb2957c2a4426a60c17b871727b1e9109
AllexisAlvarico <c00242855@itcarlow.ie>
2020-12-18 05:57:30 +0000

    added the explosion, fire particles and death particle and finished this feature

--- Lab1/Assets/Scripts/DestroyOnContact.cs
+++ Lab1/Assets/Scripts/DestroyOnContact.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class DestroyOnContact : MonoBehaviour
 {
-  
 
+    public GameObject bloodExplosion;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy"))
         {
             Destroy(other.gameObject);
+            Destroy(transform.gameObject);
+            Instantiate(bloodExplosion, transform.position, transform.rotation);
+        }
+
+        if(other.CompareTag("Wall"))
+        {
             Destroy(transform.gameObject);
         }
         
---
+++ Lab1/Assets/Scripts/Explosion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public GameObject explosion;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Wall"))
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(transform.gameObject);
+        }
+    }
+}
