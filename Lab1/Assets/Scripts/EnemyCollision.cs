d236ab4ae7e6ef151fb7553cc375834b1f232e8e
John Sinnott <C00242525@itcarlow.ie>
2021-01-25 10:22:54 +0000

    Added proper background Sprite

--- Lab1/Assets/Scripts/EnemyCollision.cs
+++ Lab1/Assets/Scripts/EnemyCollision.cs
@@ -60,7 +60,6 @@ public class EnemyCollision : MonoBehaviour
 
         if (deathTimer <= 0f)
         {
-            this.GetComponent<SpriteRenderer>().enabled = false;
             Destroy(this.gameObject);
             Debug.Log(deathTimer);
             deathTimer = .75f;
