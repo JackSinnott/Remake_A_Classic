3b1a71eaf8b21066a6bdf8e52cbb882841366b52
AllexisAlvarico <c00242855@itcarlow.ie>
2021-02-12 18:06:08 +0000

    fixing the jumping

--- Lab1/Assets/Scripts/PlayerController.cs
+++ Lab1/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,10 @@ public class PlayerController : MonoBehaviour
     // player health
     private int m_health;
 
+    //jumping var
+    private bool m_grounded;
+    public float m_jumpPower;
+
 
     void Start()
     {
@@ -23,5 +27,23 @@ public class PlayerController : MonoBehaviour
     {
         m_movement = Input.GetAxis("Horizontal");
         m_rb.velocity = new Vector2(m_movement * m_speed, m_rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && m_grounded)
+        {
+            m_rb.AddForce(new Vector2(0, m_jumpPower), ForceMode2D.Impulse); 
+            m_grounded = false; // player has jumped
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            m_grounded = true; // player is on the ground
+            /*Anim.SetBool("IsJumping", false);*/
+        }
+    }
+
+
+
 }
